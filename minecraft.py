f84eb180cd7bb45faedbb5e5bee0d33cd8c37373
Daniel Hill <daniel@enemyplanet.geek.nz>
2011-05-30 05:37:51 +1200

    custom commands including backing up enabled

--- minecraft.py
+++ minecraft.py
@@ -1,6 +1,6 @@
 print 'importing twisted'
 from twisted.application import internet, service
-from twisted.internet import reactor, protocol, defer
+from twisted.internet import reactor, protocol, defer, utils
 from twisted.protocols import basic
 from twisted.python import components
 
@@ -13,9 +13,124 @@ import os, re, random as r, shlex, time
 class LineEventer(basic.LineReceiver):
 
     delimiter = '\n'
+    service = None
+    cmd_deffered = None
+    saving = None
+    backingup = None
+    
+    def connectionMade(self):
+        self.ops = [ x[:-1].lower() for x in open(os.path.join(run_dir, 'ops.txt'))\
+                .readlines() ]
+        print self.ops
+        
+        self.re_strip = re.compile(r'^\d{4}-\d{2}-\d{2} ' + \
+                '\d{2}:\d{2}:\d{2} \[INFO\]' +\
+                ' (.+)$')
+
+        self.re_event = re.compile(r'^(\w+):? (.+)$')
+        
+        self.re_events = [
+                (re.compile(r'issued server command: (.+)'), self.cmd),
+#                (re.compile(r'Forceing save..'), self.forcing_save),
+                (re.compile(r'Save complete.'), self.save_complete),
+                ]
 
     def lineReceived(self, line):
-       print line 
+        print line
+        stripped = self.re_strip.search(line)
+        if stripped:
+            print 'stripped', stripped
+            event = self.re_event.search(stripped.group(1))
+            if event:
+                print 'event', event
+                for re, func in self.re_events:
+                    match = re.search(event.group(2))
+                    if match:
+                        print 'match', match
+                        def error(err):
+                            err.printTraceback()
+    
+                        d = defer.Deferred()
+                        d.addCallback(func, event.group(1).lower())
+                        d.addErrback(error)
+                        d.callback(match)
+                        break
+    
+    def save_complete(self, match, player):
+        print 'function save_complete called'
+        if self.saving:
+            print 'saving deferred'
+            self.saving.callback('done')           
+        else:
+            print 'saving deferred missing OH KNOW!'
+
+    def cmd(self, match, player):
+            if match:
+                cmd_args = match.group(1)[:-1].split(' ', 1)
+                try:
+                    cmd, args = cmd_args
+                except ValueError:
+                    cmd, args = cmd_args[0], None
+                
+
+            c = getattr(self, 'cmd_' + cmd, None)
+            if not c:
+                builtins =  ['say', 'kick', 'ban', 'pardon', 'ban-ip',
+                    'pardon-ip', 'op', 'deop', 'tp', 'give', 'tell',
+                    'stop', 'save-all', 'save-off', 'save-on', 'list',
+                    'time']
+                if cmd not in builtins:
+                    self.minecraft.tell(player, cmd + ' not found')
+            else:
+                d = c(player, args)
+
+    def cmd_echo(self, player, args):
+        if args:
+            return defer.succeed(
+                    self.minecraft.say(
+                        player + ': ' + args))
+        else:
+            return defer.succeed(self.minecraft.say(
+                    'well what do you want to echo?'))
+
+
+
+    def cmd_backup(self, player, args):
+
+        self.minecraft.say(player + ' issued command to backup')
+        
+        if player in self.ops:
+            self.saving = defer.Deferred()
+            self.minecraft.save_off()
+            self.minecraft.save_all()
+
+            def finished(ignore):
+                print 'tar exited with code', ignore
+                self.saving = None
+                self.backingup = None
+                self.minecraft.save_on()
+
+            def backup(ignore):
+                if not self.backingup:
+                    print 'spawning tar'
+                    d = self.backingup = utils.getProcessOutputAndValue('/bin/tar',
+                            args=['-cjf',
+                                'backups/minecraft_' + \
+                                        time.strftime('%F.%T%z') + \
+                                        '.tar.bz2',
+                                'poly'],
+                        env=os.environ,
+                        path=self.service.run_dir)
+                    d.addCallback(finished)
+                else:
+                    self.minecraft.say('already backing up!')
+
+            def failed_backup(err):
+                self.minecraft.say('backup failed')
+                err.printTraceback()
+
+            self.saving.addCallbacks(backup, failed_backup)
+        
     
 class DummyTransport:
     disconnecting = 0
@@ -29,6 +144,8 @@ class MinecraftProtocol(protocol.ProcessProtocol):
     
     def connectionMade(self):
         self.output = LineEventer()
+        self.output.minecraft = self
+        self.output.service = self.service
         self.output.makeConnection(transport)
 
     def outReceived(self, data):
@@ -45,9 +162,10 @@ class MinecraftProtocol(protocol.ProcessProtocol):
         self.service.connectionLost()
 
 
-    def say(self, msgs):
-        for msg in msgs.split('\n'):
-            self.transport.write('say ' + msg + '\n')
+    def say(self, *args):
+        for msgs in args:
+            for msg in msgs.split('\n'):
+                self.transport.write('say ' + msg + '\n')
 
     def kick(self, player):
         self.transport.write('kick ' + player + '\n')
@@ -81,8 +199,10 @@ class MinecraftProtocol(protocol.ProcessProtocol):
         else:
             self.transport.write('give ' + player + ' ' + str(id) + '\n')
 
-    def tell(self, player, msg):
-        self.transport.write('tell ' + player + ' ' + msg + '\n')
+    def tell(self, player, *args):
+        for msgs in args:
+            for msg in msgs.split('\n'):
+                self.transport.write('tell ' + player + ' ' + msg + '\n')
 
     def stop(self):
         self.transport.write('stop\n')
@@ -142,7 +262,7 @@ class MinecraftService(service.Service):
             if time.time() - self.time_started < self.threshold:
                 print 'Minecraft died too quickly, giving up!'
             else:
-                reactor.callLater(0, start_minecraft)
+                reactor.callLater(0, self.start_minecraft)
         else:
             print 'service stopping not restarting Minecraft'
             self.stopping_deferred.callback(None)
