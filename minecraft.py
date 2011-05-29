2c950e4f569302ef4582d6e402c318814023876b
Daniel Hill <daniel@enemyplanet.geek.nz>
2011-05-29 21:23:59 +1200

    so now a total rewrite, handles things better, but doesn't do as much, yet

--- minecraft.py
+++ minecraft.py
@@ -1,79 +1,53 @@
 print 'importing twisted'
-from twisted.internet import reactor, protocol, fdesc, task
-from twisted.python import log
+from twisted.application import internet, service
+from twisted.internet import reactor, protocol, defer
+from twisted.protocols import basic
+from twisted.python import components
+
+from zope.interface import Interface, implements
 
 print 'importing os'
-import os, re, random as r, shlex
-run_dir = os.path.dirname(__file__)
+import os, re, random as r, shlex, time
 
-class MinecraftServer(protocol.ProcessProtocol):
-    def __init__(self):
-        self.ops = [ x[:-1] for x in open(os.path.join(run_dir, 'ops.txt')).readlines() ]
-        print self.ops
-        self.chance = 5
-        self.re_cmd = re.compile(r'^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} \[INFO\] (?:' + '|'.join(self.ops) + ') issued server command: (.+)$', re.I)
 
+class LineEventer(basic.LineReceiver):
+
+    delimiter = '\n'
+
+    def lineReceived(self, line):
+       print line 
+    
+class DummyTransport:
+    disconnecting = 0
+
+transport = DummyTransport()
+
+class MinecraftProtocol(protocol.ProcessProtocol):
+
+    service = None
+    empty = 1
+    
     def connectionMade(self):
-        self.say('hello, world')
-        
-        self.day(False)
-
-    def day(self, yesterday, today=None):
-        def will_we_sleep_tonight():
-            return r.choice([False] * (self.chance - 1) + [True])
-
-        if today:
-            self.say('night falls! Sunrise will be slow tonight. Hold close those dear, as it\'s not so safe any more.')
-            reactor.callLater(510, self.day, today)
-        else:
-            if yesterday:
-                tired = False
-                reactor.callLater(690, self.day, today, tired)
-
-            else:
-                tired = will_we_sleep_tonight()
-                self.time('set', -900) # in game ticks
-                reactor.callLater(690, self.day, today, tired)
-            
-
-            if tired:
-                self.say('Night will fall this evening.')
-
-            else:
-                self.say('Sunrise at night fall once again!')
-
+        self.output = LineEventer()
+        self.output.makeConnection(transport)
 
     def outReceived(self, data):
-        print data
-        log.msg(data)
-        self.chew(data)
+        self.output.dataReceived(data)
+        self.empty = data[-1] == '\n'
 
-    def chew(self, data):
-        match = self.re_cmd.search(data)
-        
-        if match:
-            print 'match found'
-            cmd = shlex.split(match.group(1))
-            try:
-                {
-                    'echo': self.say,
-                    'chance': self.ch_chance,
-                }[cmd[0]](*cmd[1:])
-            except Exception, e:
-                self.say(e.__repr__())
-    def ch_chance(self, new_chance):
-        try:
-            self.chance = int(new_chance)
-            self.say('chance for night fall has been changed')
-        except:
-            self.say('error! In command: chance')
+    errReceived = outReceived
+
+    def processEnded(self, reason):
+
+        if not self.empty:
+            self.output.dataReceived('\n')
+
+        self.service.connectionLost()
+
 
     def say(self, msgs):
         for msg in msgs.split('\n'):
             self.transport.write('say ' + msg + '\n')
-
-    def update(self):
-        self.say('update needs downloading')
 
     def kick(self, player):
         self.transport.write('kick ' + player + '\n')
@@ -102,7 +76,8 @@ class MinecraftServer(protocol.ProcessProtocol):
     def give(self, player, id , num=None):
 
         if num:
-            self.transport.write('give ' + player + ' ' + str(id) + ' ' + str(num) + '\n')
+            self.transport.write('give ' + player + ' ' + str(id) + ' ' + \
+                    str(num) + '\n')
         else:
             self.transport.write('give ' + player + ' ' + str(id) + '\n')
 
@@ -126,17 +101,55 @@ class MinecraftServer(protocol.ProcessProtocol):
 
     def time(self, action, amount):
         self.transport.write('time ' + action + ' ' + str(amount) + '\n')
-    
 
-        
-print 'Creating Server interface'
-ServerProtocol = MinecraftServer()
-print 'spawning server'
-reactor.spawnProcess(ServerProtocol,
-    '/usr/bin/minecraft-server',
-    args=['minecraft-server', 'nogui'],
-    env=os.environ,
-    path=run_dir,
-    usePTY=True)
-print 'actually do that stuff'
-reactor.run()
+
+class MinecraftService(service.Service):
+    fatal_time = 1
+    active = 0
+    eventer = None
+    time_started = 0
+    threshold = 1 # secounds
+    minecraft_proc = None
+    stopping_deferred = None
+
+    def __init__(self, run_dir):
+        self.run_dir = run_dir
+
+    def startService(self):
+        service.Service.startService(self)
+        self.active = 1
+        reactor.callLater(0, self.start_minecraft)
+
+    def stopService(self):
+        service.Service.stopService(self)
+        self.active = 0
+        self.minecraft_proc.stop()
+        d = self.stopping_deferred = defer.Deferred()
+        return d
+
+    def start_minecraft(self):
+        p = self.minecraft_proc = MinecraftProtocol()
+        p.service = self
+        self.time_started = time.time()
+        reactor.spawnProcess(p, '/usr/bin/minecraft-server',
+            args=['minecraft-server', 'nogui'],
+            env=os.environ,
+            path=self.run_dir,
+            usePTY=True)
+
+    def connectionLost(self):
+        if self.active:
+            if time.time() - self.time_started < self.threshold:
+                print 'Minecraft died too quickly, giving up!'
+            else:
+                reactor.callLater(0, start_minecraft)
+        else:
+            print 'service stopping not restarting Minecraft'
+            self.stopping_deferred.callback(None)
+
+
+run_dir = os.path.dirname(__file__)
+
+application = service.Application('minecraft')
+minecraft = MinecraftService(run_dir)
+minecraft.setServiceParent(application)
